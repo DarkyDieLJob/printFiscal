ae82138993b90f61d1fc25fea2175d470c803e04
DieL <diel.job@gmail.com>
2024-06-22 15:46:30 -0300

    comit faltan los prints

---
+++ actualizar_entorno.bat
@@ -0,0 +1,22 @@
+@echo off
+cd %~dp0
+
+IF NOT EXIST "venv\" (
+    echo Creando entorno virtual...
+    python -m venv venv
+)
+
+call venv\Scripts\activate
+python -m pip install --upgrade pip
+
+REM Verifica si el archivo requirements.txt existe
+IF NOT EXIST "requirements.txt" (
+    echo Creando archivo requirements.txt vacío...
+    echo. > requirements.txt
+)
+
+pip install -r requirements.txt
+echo.
+echo ¡Actualización completada!
+pause
+
