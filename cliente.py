76d8f5d695fae598c8d6e5aff2b49ea0bb6d701d
Matías Leandro López <diel.job@gmail.com>
2024-06-23 22:35:12 -0300

    comit

--- cliente.py
+++ cliente.py
@@ -3,21 +3,36 @@ import asyncio
 import websockets
 import json
 
-async def conectar_a_websocket():
+async def conectar_a_websocket(data):
 	uri = "ws://169.254.0.251:12000/ws"  # Cambia la IP y el puerto según tu servidor
-	# Crear el JSON
-	data = {
-		"printTicket": {
-			"encabezado": ["Nombre del vendedor:", "PEPE GOMEZ", "."],
-			"cabecera": {
-				"tipo_cbte": "FA",
-				"nro_doc": 11111111111,
-				"domicilio_cliente": "CALLE DE LA DIRECCION 1234",
-				"tipo_doc": "CUIT",
-				"nombre_cliente": "JOSE LOPEZ",
-				"tipo_responsable": "RESPONSABLE_INSCRIPTO"
-			},
-			"items": [
+
+	# Convertir a formato de bytes
+	mensaje_json = json.dumps(data).encode()
+
+	async with websockets.connect(uri) as websocket:
+		await websocket.send(mensaje_json)
+		respuesta = await websocket.recv()
+		print(f"Respuesta del servidor: {respuesta}")
+
+# Ejemplo de uso
+
+# Crear el JSON
+data = {
+	"printTicket": {
+		"encabezado": [
+			"Nombre del vendedor:",
+			"PEPE GOMEZ",
+			"."
+		],
+		"cabecera": {
+			"tipo_cbte": "FA",
+			"nro_doc": 11111111111,
+			"domicilio_cliente": "CALLE DE LA DIRECCION 1234",
+			"tipo_doc": "CUIT",
+			"nombre_cliente": "JOSE LOPEZ",
+			"tipo_responsable": "RESPONSABLE_INSCRIPTO"
+		},
+		"items": [
 			{
 			"alic_iva": 21,
 			"importe": 128.57,
@@ -31,28 +46,21 @@ async def conectar_a_websocket():
 			"ds": "VINO",
 			"qty": 6
 			}
-			],
-			"formasPago": [
+		],
+		"formasPago": [
 			{
 			"ds": "Cuenta Corriente",
 			"importe": 4924.39
 			}
-			],
-			"pie": ["Efectivo 4924.39", "Vuelto: 0"]
-		},
-		"printerName": "IMPRESORA_FISCAL"
-		}
-
-	# Convertir a formato de bytes
-	mensaje_json = json.dumps(data).encode()
-
-	async with websockets.connect(uri) as websocket:
-		await websocket.send(mensaje_json)
-		respuesta = await websocket.recv()
-		print(f"Respuesta del servidor: {respuesta}")
-
-# Ejemplo de uso
-asyncio.run(conectar_a_websocket())
+		],
+		"pie": [
+			"Efectivo 4924.39",
+			"Vuelto: 0"
+		]
+	},
+	"printerName": "IMPRESORA_FISCAL"
+	}
+asyncio.run(conectar_a_websocket(data))
 
 
 
