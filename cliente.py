b02b51f84e5c9b6f47d95eb4b651cf3b164e156c
Matías Leandro López <diel.job@gmail.com>
2024-06-23 22:00:28 -0300

    comit primer config y despliegue

---
+++ cliente.py
@@ -0,0 +1,58 @@
+
+import asyncio
+import websockets
+import json
+
+async def conectar_a_websocket():
+	uri = "ws://169.254.0.251:12000/ws"  # Cambia la IP y el puerto según tu servidor
+	# Crear el JSON
+	data = {
+		"printTicket": {
+			"encabezado": ["Nombre del vendedor:", "PEPE GOMEZ", "."],
+			"cabecera": {
+				"tipo_cbte": "FA",
+				"nro_doc": 11111111111,
+				"domicilio_cliente": "CALLE DE LA DIRECCION 1234",
+				"tipo_doc": "CUIT",
+				"nombre_cliente": "JOSE LOPEZ",
+				"tipo_responsable": "RESPONSABLE_INSCRIPTO"
+			},
+			"items": [
+			{
+			"alic_iva": 21,
+			"importe": 128.57,
+			"ds": "FERNET",
+			"qty": 240,
+			"tasaAjusteInternos": 0
+			}, 
+			{
+			"alic_iva": 21,
+			"importe": 164.01,
+			"ds": "VINO",
+			"qty": 60
+			}
+			],
+			"formasPago": [
+			{
+			"ds": "Cuenta Corriente",
+			"importe": 4924.39
+			}
+			],
+			"pie": ["Efectivo 4924.39", "Vuelto: 0"]
+		},
+		"printerName": "IMPRESORA_FISCAL"
+		}
+
+	# Convertir a formato de bytes
+	mensaje_json = json.dumps(data).encode()
+
+	async with websockets.connect(uri) as websocket:
+		await websocket.send(mensaje_json)
+		respuesta = await websocket.recv()
+		print(f"Respuesta del servidor: {respuesta}")
+
+# Ejemplo de uso
+asyncio.run(conectar_a_websocket())
+
+
+
