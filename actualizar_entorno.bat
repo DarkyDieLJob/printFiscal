5d141564bec8d2194f04867f4c0ccffcaa47639e
Matías Leandro López <diel.job@gmail.com>
2024-06-25 20:22:11 -0300

    comit

--- actualizar_entorno.bat
+++ actualizar_entorno.bat
@@ -5,34 +5,10 @@ IF NOT EXIST "venv_3-10-10\" (
     echo Creando entorno virtual...
     python -m venv venv_3-10-10
 )
-echo ¡1!
-pause
 
-IF EXIST "venv_3-10-10\" (
-    call venv_3-10-10\Scripts\activate
-    python -m pip install --upgrade pip
-    pip install -r requirements.txt
-    echo ¡Actualización completada!
-)
-echo ¡1.5!
-pause
-
-
-IF NOT EXIST "venv_2-7-12\" (
-    .\Python27\python.exe -m pip install --upgrade pip
-    .\Python27\python.exe -m pip install virtualenv
-    echo Creando entorno virtual...
-    .\Python27\python.exe -m venv venv_2-7-12
-)
-echo ¡2!
-pause
-
-IF EXIST "venv_2-7-12\" (
-    .\Python27\python.exe -m pip install virtualenv
-    call venv_2-7-12\Scripts\activate
-    .\Python27\python.exe -m pip install -r requirements_fiscal.txt
-    echo.
-    echo ¡Actualización completada!
-)
-echo ¡2.5!
+call venv\Scripts\activate
+python -m pip install --upgrade pip
+python pip install -r requirements.txt
+echo.
+echo ¡Actualización completada!
 pause
